c12a4db168921c90dcbd3dfd955960c36089271a
Avanishsri31 <90971881+Avanishsri31@users.noreply.github.com>
2024-03-02 23:34:37 +0530

    Update

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -10,15 +10,15 @@ function page() {
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8 text-white">All courses ({courseData.courses.length})</h1>  
         <div className="flex flex-wrap justify-center">
             {courseData.courses.map((course) => (
-                <CardContainer className="inter-var m-4">
-                <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
-                  <CardItem
+                <CardContainer  className="inter-var m-4">
+                <CardBody  className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+                  <CardItem key={course.title}
                     translateZ="50"
                     className="text-xl font-bold text-neutral-600 dark:text-white"
                   >
                     {course.title}
                   </CardItem>
-                  <CardItem
+                  <CardItem key={course.description}
                     as="p"
                     translateZ="60"
                     className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
